7b1a74b9f20fcbc40a9464825d8c981645a1fd08
liuwanlin <954408050>
2016-12-22 17:04:14 +0800

    调整代码结构

--- www/js/app.js
+++ www/js/app.js
@@ -61,22 +61,19 @@ var DateTools = {
         return  +new Date(date) - 86400000;
     },
     /**
-     * 获取下一天
-     * @return {[string]} 格式 2016-11-06
+     * 获取下一天, 格式 2016-11-06
      */
     getNextDateStr: function(date) {
         return this.getDate(new Date(this.getNextDateTSP(date)));
     },
     /**
-     * 获取上一天
-     * @return {[string]} 格式 2016-11-06
+     * 获取上一天, 格式 2016-11-06
      */
     getPreDateStr: function(date) {
         return this.getDate(new Date(this.getPreDateTSP(date)));
     },
     /**
-     * 获取 2016-11-06 格式日期
-     * @param  {[Date]} date
+     * 获取当前日期, 2016-11-06 格式日期
      */
     getDate: function(date) {
         var m = date.getMonth() + 1;
@@ -85,31 +82,29 @@ var DateTools = {
     }
 };
 
+var APIs = {
+    "latest" : "http://news-at.zhihu.com/api/4/news/latest", // 最新消息
+    "hot" : "http://news-at.zhihu.com/api/3/news/hot", // 热门消息
+    "before" : "http://news-at.zhihu.com/api/4/news/before/#{para}", // 过往消息, para 格式 20131119, 获取的消息为上一天, 即 20131118 消息
+    "news" : "http://news-at.zhihu.com/api/4/news/#{para}", // 消息内容
+    "themes" : "http://news-at.zhihu.com/api/4/themes", // 主题日报列表
+    "theme" : "http://news-at.zhihu.com/api/4/theme/#{para}", // 主题日报内容
+    "sections" : "http://news-at.zhihu.com/api/3/sections", // 栏目总览
+    "section" : "http://news-at.zhihu.com/api/3/section/#{para}", // 栏目具体消息
+    "story-extra" : "http://news-at.zhihu.com/api/4/story-extra/#{para}", // 额外信息
+    "long-comments" : "http://news-at.zhihu.com/api/4/story/#{para}/long-comments", // 新闻对应长评论
+    "short-comments": "http://news-at.zhihu.com/api/4/story/#{para}/short-comments", // 新闻对应短评论
+    "recommenders" : "http://news-at.zhihu.com/api/4/story/#{para}/recommenders", // 新闻的推荐者
+}
+
 var ZhihuDaily = {
     is_reading: false, // 是否正在读取数据
-    APIs: {
-        "latest"        : "http://news-at.zhihu.com/api/4/news/latest",                     // 最新消息
-        "hot"           : "http://news-at.zhihu.com/api/3/news/hot",                        // 热门消息
-        "before"        : "http://news-at.zhihu.com/api/4/news/before/#{para}",             // 过往消息, para 格式 20131119, 获取的消息为上一天, 即 20131118 消息
-        "news"          : "http://news-at.zhihu.com/api/4/news/#{para}",                    // 消息内容
-        "themes"        : "http://news-at.zhihu.com/api/4/themes",                          // 主题日报列表
-        "theme"         : "http://news-at.zhihu.com/api/4/theme/#{para}",                   // 主题日报内容
-        "sections"      : "http://news-at.zhihu.com/api/3/sections",                        // 栏目总览
-        "section"       : "http://news-at.zhihu.com/api/3/section/#{para}",                 // 栏目具体消息
-        "story-extra"   : "http://news-at.zhihu.com/api/4/story-extra/#{para}",             // 额外信息
-        "long-comments" : "http://news-at.zhihu.com/api/4/story/#{para}/long-comments",     // 新闻对应长评论
-        "short-comments": "http://news-at.zhihu.com/api/4/story/#{para}/short-comments",    // 新闻对应短评论
-        "recommenders"  : "http://news-at.zhihu.com/api/4/story/#{para}/recommenders",      // 新闻的推荐者
-    },
     DATE_SUFFIX: " 06:59:58", // api 日期中固定时间
     AJAX_GET_TIME_OUT: 5000, // ajax 请求过期时间
     IS_SHOW_LOADING: true, // 是否显示 loading 提示
     LOADING_RES_HEIGHT: 20, // loading 距离底部位置多少时开始加载, 单位 px
     /**
      * 替换 api 中 #{para} 参数
-     * @param  {[string]} api 带 #{para} 参数的 url
-     * @param  {[string]} para 参数
-     * @return {[string]} 完整 api url
      */
     getAPIURL: function(api, para) {
         return api.replace('#{para}', para);
@@ -117,7 +112,6 @@ var ZhihuDaily = {
     /**
      * 获取开始时间时间戳, 用于 api 中时间戳参数
      * @param  {[string]} date 日期, 格式 2016-11-06
-     * @return {[string]} 时间戳
      */
     getStartTimeTSP: function(date) {
         return +new Date(date + this.DATE_SUFFIX);
@@ -125,22 +119,21 @@ var ZhihuDaily = {
     /**
      * 获取 '过往消息' 消息信息, 这个方法将调整 api 中获取的是上一天消息的问题
      * @param  {[type]} date 当前日期
-     * @return {[type]}
      */
     getBeforeNewsObj: function(date) {
-        return this.ajaxGet(this.getAPIURL(this.APIs.before, DateTools.getNextDateStr(date).replace(/-/g, '')));
+        return this.ajaxGet(this.getAPIURL(APIs.before, DateTools.getNextDateStr(date).replace(/-/g, '')));
     },
     getLatestNewsObj: function() {
-        return this.ajaxGet(this.APIs.latest);
+        return this.ajaxGet(APIs.latest);
     },
     getHotNewsObj: function() {
-        return this.ajaxGet(this.APIs.hot);
+        return this.ajaxGet(APIs.hot);
     },
     getThemesObj: function() {
-        return this.ajaxGet(this.APIs.themes);
+        return this.ajaxGet(APIs.themes);
     },
     getSectionsObj: function() {
-        return this.ajaxGet(this.APIs.sections);
+        return this.ajaxGet(APIs.sections);
     },
     /**
      * ajax get 同步获取信息
@@ -172,7 +165,6 @@ var ZhihuDaily = {
     },
     /**
      * 加入消息至主页页面
-     * @param  {[type]} storiesObj [description]
      * @param  {[type]} date 显示日期 xxxx-xx-xx
      */
     appendNews2Stories: function(storiesObj, date) {
